f44bee054ebb86acf19d961dc050f0cf289af29c
VisheshPandita <vishesh.pndta@gmail.com>
2019-06-18 13:16:06 +0530

    learning R^2 method

--- Machine Learning/Part 2 - Regression/Section 4 - Simple Linear Regression/sml.py
+++ Machine Learning/Part 2 - Regression/Section 4 - Simple Linear Regression/sml.py
@@ -1,8 +1,13 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pickle
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
@@ -17,20 +22,45 @@ df.fillna(-9999, inplace=True)
 forcast_out = int(math.ceil(0.01*len(df)))
 
 df['label'] = df[forcast_col].shift(-forcast_out)
-df.dropna(inplace=True)
 
 
 X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X = X[:-forcast_out]
+X_lately = X[-forcast_out:]
+
+df.dropna(inplace=True)
+
 Y = np.array(df['label'])
 
-X = preprocessing.scale(X)
-# X = X[:-forcast_out+1]
-# df.dropna(inplace=True)
-y = np.array(df['label'])
-
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X,Y, test_size=0.2)
-clf = LinearRegression()
-clf.fit(X_train, Y_train)
-accuracy = clf.score(X_test, Y_test)
+# clf = LinearRegression()
+# clf.fit(X_train, Y_train)
+# with open('linearregression.pickle', 'wb') as f:
+#     pickle.dump(clf, f)
 
-print(accuracy)
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
+accuracy = clf.score(X_test, Y_test)
+forcast_set = clf.predict(X_lately)
+
+print(forcast_set, accuracy, forcast_out)
+
+df['Forcast'] = np.nan
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forcast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forcast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()    
